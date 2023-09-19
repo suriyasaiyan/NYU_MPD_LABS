fdd3aea46fa3a6f972d0583673422727ba8efc19
suriyasaiyan <72756153+suriyasaiyan@users.noreply.github.com>
2023-09-18 21:42:48 -0400

    update on lab2
    answered the makenote ques and completed the testing on lab2 and rectified a mistake

--- lab2/ReadMe.md
+++ lab2/ReadMe.md
@@ -1,3 +1,3 @@
 # Week 2 Lab: System Verilog
-
-Fill out this readme as required by lab guidance
+## makeNote:
+- The choice of approach to use depends on the specific requirements, constraints, and goals of the testing process. In my opinion a combination of these approaches, along with other testing techniques, is good to maintain the balance in the process.
--- lab2/rtl/Exercise2.sv
+++ lab2/rtl/Exercise2.sv
@@ -10,13 +10,14 @@ module Exercise2 (
     input clk,
     input nReset,
     input [15:0] init,
-    output reg [15:0] out
+    output logic [15:0] out
 );
-    always @(posedge clk or posedge nReset) begin
+    always @(posedge clk) begin
       if(!nReset)
         out <= init;
       else
         out <= {out[14:0], out[15] ^ out[13] ^ out[12] ^ out[10]};
     end
 endmodule
+
 
